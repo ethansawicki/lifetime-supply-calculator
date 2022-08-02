692577efe6c543d2f1eb004317d64a9ab356c0df
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-02 00:52:42 -0500

    added JS files to github

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const currentAge = 32
+const maxAge = 70
+const beefJerkyPerDay = 1
+const yearsForSnacc = maxAge - currentAge
+const lifetimeAmountSnacc = 365 * yearsForSnacc
+
+console.log(`You will need ${lifetimeAmountSnacc} bags of Beef Jerky (1 per day) to last you until the ripe old age of ${maxAge}`)
